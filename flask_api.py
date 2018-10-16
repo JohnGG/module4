df8d8c725a94efb5fcb378a257397ca6a179983e
baptiste <baptiste@mindee.co>
2018-10-16 16:26:00 +0200

    added graph definition and loading

--- flask_api.py
+++ flask_api.py
@@ -1,35 +1,30 @@
 from flask import Flask
 import json
+import tensorflow as tf
 
 app = Flask(__name__)
 
-COMPANIES = {
-    1: {
-        'name': 'mindee',
-        'employees': 10,
-        'address': 'bld raspail'
-    },
-    2: {
-        'name': 'apple',
-        'employees': 10000,
-        'address': '1 infinite loop'
-    }
-}
+# This could be added to the Flask configuration
+MODEL_PATH = './model.pb'
+
+# Read the graph definition file
+with open(MODEL_PATH, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+
+# Load the graph stored in `graph_def` into `graph`
+graph = tf.Graph()
+with graph.as_default():
+    tf.import_graph_def(graph_def, name='')
+    nodes = [n.name for n in tf.get_default_graph().as_graph_def().node]
+    for no in nodes:
+        print(no)
+
+# Enforce that no new nodes are added
+graph.finalize()
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-@app.route("/ping")
-def ping():
-    return "pong"
-
-@app.route("/companies")
-def companies():
-    return json.dumps(list(COMPANIES.values()))
-
-@app.route("/companies/<int:id_>")
+@app.route("/predict")
 def company(id_):
     try:
         return json.dumps(COMPANIES[id_])
