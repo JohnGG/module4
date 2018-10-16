9d83213b7455b9637bd4cba6bd3d9e9bc6bc597a
baptiste <baptiste@mindee.co>
2018-10-16 16:49:23 +0200

    added images for inference and flas_api

--- flask_api.py
+++ flask_api.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 import tensorflow as tf
+import numpy as np
 
 app = Flask(__name__)
 
@@ -23,11 +24,27 @@ with graph.as_default():
 # Enforce that no new nodes are added
 graph.finalize()
 
+# Create tensorflow session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(graph=graph, config=config)
+
+
+# Get the input and output operations
+input_op = graph.get_operation_by_name('inputs')
+input_tensor = input_op.outputs[0]
+output_op = graph.get_operation_by_name('pred')
+output_tensor = output_op.outputs[0]
+
 
 @app.route("/predict")
-def company(id_):
+def company():
     try:
-        return json.dumps(COMPANIES[id_])
+        # Generate placeholders nodes
+        placeholders = {input_tensor: [np.zeros(shape=(50, 50, 3))]}
+        preds = sess.run(output_tensor, placeholders)
+        print(preds)
+        return json.dumps({})
     except KeyError:
         return json.dumps({'error': 'Company not found', 'code': 'err_not_found'}), 404
 
